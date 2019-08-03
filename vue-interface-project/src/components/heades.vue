2d5d451c2831a4d39696b95a925e5e8700bfd68d
王海林 <wanghailin@baice100.com>
2019-08-03 12:07:59 +0800

    增加注释

---
+++ vue-interface-project/src/components/heades.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="main-head">
+        <div class="head-img">
+            <img :src="vueImg" height="50px" width="50px" alt="接口测试图片"/>
+            <span>接口测试系统</span>
+        </div>
+    </div>
+</template>
+
+<script>
+  import img from '../assets/interface.png'
+
+  export default {
+    name: 'header',
+    data () {
+      return {
+        vueImg: img
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    .main-head {
+        background: #eff4fa;
+        height: 60px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 20px;
+    }
+
+    .head-img {
+        display: flex;
+        align-items: center;
+    }
+</style>
--- vue-interface-project/src/views/Login.vue
+++ vue-interface-project/src/views/Login.vue
@@ -1,11 +1,21 @@
 <template>
     <div class="login">
+        <!--        vue的组件是如何使用：-->
+        <!--&lt;!&ndash;        1. 创建vue组件&ndash;&gt;components/header.vue-->
+        <!--                    2. 在父组件里面导入组件的路径-->
+        <!--                    3. 在父组件里面的components进行注册-->
+        <!--                    4. 然后在父组件里面的html部分按照定义的key当做标签来使用-->
         <header-component></header-component>
         <div class="login-context">
             <h2>账号登录</h2>
 
+            <!--            rules 校验规则 ，model绑定数据，子数据el-form-item的 v-model是绑定数据的属性-->
+
+            <!--            ref唯一id找出元素对象，vue使用ref，js使用id-->
+            <!--             1、先在元素里定义ref为XXX，然后在vue使用 this.$refs.XXX 就可以获取元素对象-->
+
             <el-form label-position="left" :model="loginForm" :rules="rules" ref="loginFormRef" label-width="50px" class="demo-ruleForm">
-                <el-form-item label="名称" prop="name">
+                <el-form-item label="用户名" prop="name">
                     <el-input v-model="loginForm.name" placeholder="请输入用户名"></el-input>
                 </el-form-item>
                 <el-form-item label="密码" prop="passWord">
@@ -22,9 +32,11 @@
 </template>
 
 <script>
-    import header from  "../components/header"
+  // 2. 在父组件里面导入组件的路径-->
+  import header from '../components/header'
     export default {
         name: 'login',
+      // 3.在父组件里面的components进行注册 ，header-component 作为引入名
         components: {
             "header-component": header,
         },
@@ -35,13 +47,15 @@
                     name: "",
                     passWord: "",
                 },
+              // el-form-item 的prop写rules的属性名
+
                 rules: {
                     name: [
-                        {required: true, message: '请输入用户名称', trigger: 'blur'},
+                      { required: true, message: '请输入用户名!', trigger: 'blur' },
                         {min: 1, max: 30, message: '长度在 1 到 30 个字符', trigger: 'blur'}
                     ],
                     passWord: [
-                        {required: true, message: '请输入用户密码', trigger: 'blur'},
+                      { required: true, message: '请输入用户密码!', trigger: 'blur' },
                         {min: 1, max: 30, message: '长度在 1 到 30 个字符', trigger: 'blur'}
                     ],
                 },
@@ -50,12 +64,17 @@
         },
         methods: {
             login() {
-                this.$refs.loginFormRef.validate((valid) => {
+              // Vue3.0 console.log()语法报错 只需要改成window.console.log() 即可解决
+              window.console.log(this.$refs.loginFormRef)
+              this.$refs.loginFormRef.validate((valid) => {
                     if (valid) { // 代表校验通过
+                      alert('校验通过')
                     } else {  //校验失败
-                        return false;
+                      alert('校验失败')
+                      return false
                     }
                 });
+              // this.validForm();
             },
             register() {
                 this.$refs.loginFormRef.validate((valid) => {
@@ -64,7 +83,18 @@
                         return false;
                     }
                 });
-            }
+            },
+          // 初级的表单验证
+          // validForm(){
+          //   if ("" == this.loginForm.name){
+          //     alert("用户名不能为空")
+          //     return
+          //   }
+          //   if ("" == this.loginForm.passWord){
+          //     alert("密码不能为空")
+          //     return
+          //   }
+          // }
         },
         created() {
 
@@ -80,9 +110,7 @@
         margin-right: auto;
         border-radius: 2px;
         border: 1px solid #a0b1c4;
-
         margin-top: 50px;
-
         padding: 10px 30px;
     }
 
@@ -90,4 +118,6 @@
         display: flex;
         justify-content: space-between;
     }
+
+
 </style>
